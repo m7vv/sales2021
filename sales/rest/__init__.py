0cfcf30d6dcb28bb6c6dd08a0ab05d9b1f1dffef
mvv <mvv7mvv@gmail.com>
2021-05-16 10:13:42 -0700

    rest api for films added

--- sales/rest/__init__.py
+++ sales/rest/__init__.py
@@ -1,8 +1,9 @@
 from flask_restful import Api
 from .smoke import Smoke
+from .films import FilmListApi
 
 
 def init_app(app):
     api = Api(app)
-
     api.add_resource(Smoke, '/m')
+    api.add_resource(FilmListApi, '/films', '/films/<uuid>', strict_slashes=False)
