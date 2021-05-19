72e7d98b68b68204fc4c04000205b093d28ecc3a
mvv <mvv7mvv@gmail.com>
2021-05-18 17:57:02 -0700

    restfull for foods

--- sales/rest/__init__.py
+++ sales/rest/__init__.py
@@ -1,9 +1,11 @@
 from flask_restful import Api
 from .smoke import Smoke
 from .films import FilmListApi
+from .foods import FoodsApi
 
 
 def init_app(app):
     api = Api(app)
     api.add_resource(Smoke, '/m')
     api.add_resource(FilmListApi, '/films', '/films/<uuid>', strict_slashes=False)
+    api.add_resource(FoodsApi, '/foods', '/foods/<uuid>', strict_slashes=False)
