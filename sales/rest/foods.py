be21108991c58194fa422bed17708562f93012f1
mvv <mvv7mvv@gmail.com>
2021-06-09 15:09:42 -0700

    logging for food operation

--- sales/rest/foods.py
+++ sales/rest/foods.py
@@ -3,6 +3,14 @@ from flask_restful import Resource
 
 from sales.models import db
 from sales.models.foods import Food
+import logging
+
+logging.basicConfig(filename='sales.log',
+                    encoding='utf-8',
+                    level=logging.DEBUG,
+                    format='%(levelname)s %(asctime)s %(message)s',
+                    datefmt='%m/%d/%Y %I:%M:%S %p'
+                    )
 
 
 class FoodsApi(Resource):
@@ -19,6 +27,7 @@ class FoodsApi(Resource):
     def post(self):
         food_json = request.json
         if not food_json:
+            logging.error('Attempt to create  food without json')
             return {'message': 'Wrong data'}, 400
         try:
             food = Food(
@@ -28,7 +37,9 @@ class FoodsApi(Resource):
             db.session.add(food)
             db.session.commit()
         except (ValueError, KeyError):
+            logging.error(f'Food was not created due wrong data')
             return {'message': 'Wrong data'}, 400
+        logging.info(f'Food with uuid {food.uuid} was created')
         return {'message': 'Created successfully', 'uuid': food.uuid}, 201
 
     def put(self, uuid):
@@ -44,7 +55,9 @@ class FoodsApi(Resource):
             )
             db.session.commit()
         except (ValueError, KeyError):
+            logging.error(f'Food with uuid {uuid} was not updated due wrong data')
             return {'message': 'Wrong data'}, 400
+        logging.info(f'Food with uuid {uuid} was updated')
         return {'message': 'Updated successfully'}, 200
 
     def patch(self, uuid):
@@ -62,6 +75,7 @@ class FoodsApi(Resource):
 
         db.session.add(food)
         db.session.commit()
+        logging.info(f'Food with uuid {uuid} was updated')
         return {'message': 'Updated successfully'}, 200
 
     def delete(self, uuid):
@@ -70,4 +84,5 @@ class FoodsApi(Resource):
             return "", 404
         db.session.delete(food)
         db.session.commit()
+        logging.info(f'Food with uuid {uuid} was deleted')
         return '', 204
